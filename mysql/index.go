f6ece409de09932f78d381038e87cd3da859521b
xusz <xusz@mvalley.io>
2020-05-21 23:32:19 +0800

    [ADD]mysql, just reference

---
+++ mysql/index.go
@@ -0,0 +1,14 @@
+package mysql
+
+// 官网
+// https://www.mysql.com/
+// 官网索引：
+// https://dev.mysql.com/doc/refman/8.0/en/multiple-column-indexes.html
+// 索引合并优化
+// https://dev.mysql.com/doc/refman/8.0/en/index-merge-optimization.html
+
+// 多个单列索引和联合索引的区别详解
+// https://blog.csdn.net/Abysscarry/article/details/80792876
+
+// MySQL中可为空的字段设置为NULL还是NOT NULL
+// https://www.jb51.net/article/157913.htm
